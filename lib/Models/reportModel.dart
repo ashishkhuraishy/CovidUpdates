b833fcf14c1d28ad109c5bb7631bd8511707be43
Ashish Khuraishy <ashishkhuraishy@gmail.com>
2020-03-20 18:25:04 +0530

    Implemented Core Model

---
+++ lib/Models/reportModel.dart
@@ -0,0 +1,10 @@
+class Report{
+  int recovered;
+  int confirmed;
+  int deaths;
+  int totalCases;
+  String updatedTime;
+
+  Report({this.confirmed, this.recovered, this.deaths, this.totalCases, this.updatedTime});
+
+}
---
+++ lib/Services/reports.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:covid_updates/Models/reportModel.dart';
+import 'package:http/http.dart';
+import 'package:intl/intl.dart';
+
+final String link = "https://corona.lmao.ninja/";
+
+Future<Report> getReport() async {
+  Report report;
+  DateFormat formatter = DateFormat("MMM d y").add_jm();
+
+  try {
+    Response response = await get(link + "all");
+    Map data = jsonDecode(response.body);
+    report = new Report(
+      deaths: data["deaths"],
+      recovered: data["recovered"],
+      totalCases: data["cases"],
+      confirmed: data['cases'] - (data['deaths'] + data['recovered']),
+      updatedTime: formatter
+          .format(DateTime.fromMillisecondsSinceEpoch(data['updated'])),
+    );
+    print(data);
+  } catch (e) {
+    print("Exception Caught at getReport() $e");
+  }
+
+  return report;
+}
